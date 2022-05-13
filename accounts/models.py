d64f24f9cc536ccfee4edfbe61b326e1114f24bd
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2022-05-13 15:02:49 +0300

    create singal when create user to add profile auto

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,9 @@
 from tkinter import N
+from venv import create
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 
@@ -10,3 +13,14 @@ class Profile(models.Model):
     dob = models.DateField()
     address = models.CharField(max_length=100, null=True, blank=True)
     phone_number = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.user)
+
+
+@receiver(post_save, sender=User)
+def create_profile_account_after_create_user_account(sender, instance, created, ** kwargs):
+    if created:
+        Profile.objects.create(
+            user=instance
+        )
