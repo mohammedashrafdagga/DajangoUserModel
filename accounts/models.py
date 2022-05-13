54b273a4577ecb54778a8a9af2fe514841b41783
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2022-05-13 14:51:12 +0300

    create profile model extends from user model in django and overrideed

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
+from tkinter import N
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+# create profile Model
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    dob = models.DateField()
+    address = models.CharField(max_length=100, null=True, blank=True)
+    phone_number = models.CharField(max_length=100, null=True, blank=True)
